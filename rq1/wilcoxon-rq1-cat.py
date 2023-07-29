815e94968c91ce5c49aebb64de062563b4aeb247
czlaughlin404 <63917914+czlaughlin404@users.noreply.github.com>
2023-07-29 13:20:25 -0400

    Add files via upload

---
+++ rq1/wilcoxon-rq1-cat.py
@@ -0,0 +1,28 @@
+import sys
+import pandas as pd
+import numpy as np
+from scipy.stats import wilcoxon
+
+import scipy.stats as stats
+
+pd.options.display.float_format = '{:.10f}'.format
+
+columns = ["cat","item_id","timetamp" , "target_value","pre_prediction","post_prediction","pre","post"]
+
+df1 = pd.read_csv(sys.argv[1],
+    names = columns,
+    low_memory=True,
+    header=1
+    )
+
+for c in df1.cat.unique():
+
+    df = df1[df1['cat']==c]
+
+    print(c, df.shape)
+
+    print('mean',df['pre'].mean())
+    print('mean',df['post'].mean())
+
+    stat, p = wilcoxon(df['pre'], df['post'])
+    print(c, 'Wilcoxon Statistics=%.5f, p=%.5f' % (stat, p))
